224a4218b5ca214013e867e4f79ccccd24a10956
anovak <anovak610@gmail.com>
2024-04-20 16:47:59 -0500

    update main comments

--- main.cpp
+++ main.cpp
@@ -12,27 +12,27 @@ int main() {
         // right here is where to add on character // added on 04/08/24
         Heroes::make_wizard(hero);
 
-        for (int i = 0; i < 4; ++i) {
+        for (int i = 0; i < 4; ++i) {  // create five monsters
             std::shared_ptr<Entity> monster = engine.create_monster();
             Monsters::make_demon_tiny(monster);
         }
 
-        for (int i = 0; i < 11; ++i) {
+        for (int i = 0; i < 9; ++i) {  // create ten monsters
             std::shared_ptr<Entity> monster = engine.create_monster();
             Monsters::make_muddy(monster);
         }
 
-        for (int i = 0; i < 2; ++i) {
+        for (int i = 0; i < 2; ++i) {  // create three monsters
             std::shared_ptr<Entity> monster = engine.create_monster();
             Monsters::make_orc_shaman(monster);
         }
 
-        for (int i = 0; i < 5; ++i) {
+        for (int i = 0; i < 4; ++i) { // create five monsters
             std::shared_ptr<Entity> monster = engine.create_monster();
             Monsters::make_skeleton(monster);
         }
 
-        for (int i = 0; i < 2; ++i) { // create one monster
+        for (int i = 0; i < 1; ++i) { // create one monster
             std::shared_ptr<Entity> monster = engine.create_monster();
             Monsters::make_wizard(monster);
         }
